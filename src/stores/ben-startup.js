bcdba6de35f063cdfe64c80cda36b839164691c5
Ben Roth <spongebucket@gmail.com>
2024-08-13 16:24:55 -0700

    load extensions and videos locally

--- src/stores/ben-startup.js
+++ src/stores/ben-startup.js
@@ -1,10 +1,11 @@
 export const BEN_INITIAL_CODE = `
-await loadScript("https://hyper-hydra.glitch.me/hydra-arithmetics.js")
-await loadScript('https://cdn.jsdelivr.net/npm/hydra-midi@latest/dist/index.js')
+await loadScript('/extensions/hydra-arithmetics.js')
+await loadScript('/extensions/hydra-midi.js')
 await midi.start({ input: '*', channel: '*' })
 
 shape(3,.5).diff(shape(3,.49))
 .scale(() => 0.8 + a.fft[0] * .1)
 .scale(1,.6,1)
+.mult(cc(15))
 .out()
 `;
